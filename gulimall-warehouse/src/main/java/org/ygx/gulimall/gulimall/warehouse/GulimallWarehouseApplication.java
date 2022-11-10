c2b75ea634e00309c408411808020268f07ac407
ygx <1241749920@qq.com>
2022-11-10 22:50:08 +0800

    project init
    项目初始化结构创建

---
+++ gulimall-warehouse/src/main/java/org/ygx/gulimall/gulimall/warehouse/GulimallWarehouseApplication.java
@@ -0,0 +1,13 @@
+package org.ygx.gulimall.gulimall.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallWarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallWarehouseApplication.class, args);
+    }
+
+}
---
+++ gulimall-warehouse/src/test/java/org/ygx/gulimall/gulimall/warehouse/GulimallWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ygx.gulimall.gulimall.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
