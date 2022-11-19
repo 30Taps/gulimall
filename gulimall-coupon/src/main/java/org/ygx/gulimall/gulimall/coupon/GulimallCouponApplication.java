574d3288fc7319dd629069925c9900d1c5e94302
ygx <1241749920@qq.com>
2022-11-19 16:41:42 +0800

    gateway
    网关配置及测试

--- gulimall-coupon/src/main/java/org/ygx/gulimall/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/org/ygx/gulimall/gulimall/coupon/GulimallCouponApplication.java
@@ -10,6 +10,20 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * 2.bootstrap.properties
  * 3.@Value , @RefreshScope
  * 4.去配置中心新建"模块名.properties"，把配置内容搬家
+ *
+ * 命名空间：默认public（保留空间）
+ * 在nacos创建新的命名空间会生成uuid
+ * bootstrap
+ * config.namespace="uuid"
+ *
+ * 配置组:默认DEFAULT_GROUP
+ * bootstrap
+ * config.group
+ *
+ * 加载多个配置集：避免application过大可读性差，分开放入nacos配置中心
+ * config.ext-config[i].data-id=
+ *                     .group=
+ *                     .refresh=
  */
 
 @EnableDiscoveryClient
---
+++ gulimall-gateway/src/test/java/org/ygx/gulimall/gulimallgateway/GulimallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ygx.gulimall.gulimallgateway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
